c9d7a1addcafe20bbca32cdb6f74a3bf9a7f4f8a
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-04-24 00:17:32 -0300

    EntregaFinalReact

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -8,16 +8,31 @@ export default function Checkout() {
   console.log(errors);
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
-      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
-      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-      <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
-      <input type="text" placeholder="Card number" {...register("Card number", {})} />
-      <input type="datetime" placeholder="Expiration date" {...register("Expiration date", {})} />
-      <input type="text" placeholder="CCV" {...register("CCV", {})} />
+    <div className="container-fluid animate__animated animate__fadeInRight" id='formContainer'>
+      <h1 className="display-4" id='formHeader'>Checkout Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="row" id='formInput'>
+          <div className="col">
+            <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
+          </div>
+          <div className="col">
+            <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
+          </div>
+        </div>
+        <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+        <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+        <div className="row" id='formInput'>
+          <div className="col">
+            <input type="text" placeholder="Card number" {...register("Card number", {})} />
+          </div>
+          <div className="col">
+            <input type="datetime" placeholder="Expiration date" {...register("Expiration date", {})} />
+          </div>
+        </div>
+        <input type="text" placeholder="CCV" {...register("CCV", {})} />
 
-      <input type="submit" />
+        <input type="submit" />
     </form>
+    </div>
   );
 }
