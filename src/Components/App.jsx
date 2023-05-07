f2651962c83252b1bc2fb95494965212f09e2786
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-05-07 18:10:22 -0300

    Entrega Final Schallibaum

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -7,7 +7,6 @@ import ItemDetailContainer from './ItemDetailContainer/ItemDetailContainer'
 import Cart from './Cart/Cart';
 import Checkout from './Checkout/Checkout';
 //import { createGames } from '../firebase/firebase'
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -16,26 +15,15 @@ function App() {
   return (
     <div className="App">
     <BrowserRouter>
-    <NavBar />
-    <ToastContainer position="top-center"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
-    />
-    <Routes>
-    <Route path='/' element={<ItemListContainer greeting={"Welcome!"}/>}/>
-    <Route path='/category/:category' element={<ItemListContainer/>}/>
-    <Route path='/juego/:id' element={<ItemDetailContainer/>}/>
-    <Route path='/cart' element={<Cart/>}/>
-    <Route path='*' element={<h1>404 - Not Found</h1>}/>
-    <Route path='/checkout' element={<Checkout/>}/>
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<ItemListContainer greeting={"Welcome!"}/>}/>
+        <Route path='/category/:category' element={<ItemListContainer/>}/>
+        <Route path='/juego/:id' element={<ItemDetailContainer/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='*' element={<h1>404 - Not Found</h1>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+      </Routes>
     </BrowserRouter>
     </div>
     );
